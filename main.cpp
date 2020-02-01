4df16019880e18a2dd742bd4980c02f03fc7fbef
SgtR0ck <chet.lockwood@my.tccd.edu>
2020-02-01 17:50:02 +0000

    Finished the Program

--- main.cpp
+++ main.cpp
@@ -1,13 +1,8 @@
 /*
-	Input Validation with Ref Functions 
-	A cleaner way to do input validation
-	Validating input, an essential process.
-	Source: http://stackoverflow.com/questions/514420/how-to-validate-numeric-input-c
-	Keeping Dr. McMillan from breaking my code, defend against "dark side of the force",
-	and/or legitimate user error. 
-	//Try and Catch blocks are used in error checking
 	Teacher: Dr. Tyson McMillan, Ph.D.  
-  STUDENT (McMillanite):
+  STUDENT (McMillanite): Chet Lockwood
+  IDE: Repl.it
+  This is a program to write arithmatic to variables and use if statements
 */
 #include <cstdlib>  // Provides EXIT_SUCCESS
 #include <iostream>  // Provides cout, cerr, endl
@@ -19,90 +14,72 @@ using namespace std;
 
 int main()
 {
-   //***********Validating int data type******************/
-	int input = 0;
-	input = validateInt(input); //validate my integer by function call, save that value into input
-    cout << "You entered valid int: " << input << endl;
-    
-	/****************Extend code to Validate double (follow int pattern)**********/
-	double inputDouble = 0.0;
-  	inputDouble = validateDouble(inputDouble); //validate my double by function call, save that value into inputDouble
-    cout << "You entered valid double: " << inputDouble << endl;
+//Variable declaration and initilization
+double number1 = 0.0, number2 = 0.0;
+int number3 = 0, number4 = 0;
+double addition = 0.0, subtraction = 0.0;
+double multiplication = 0.0, division = 0.0;
+int modulus = 0;
 
-	/****************Extend code to Validate char (follow int pattern)**********/
-	char inputChar = '\0'; //{0}
-	inputChar = validateChar(inputChar); //validate my char by function call, save that value into inputChar
-    cout << "You entered valid char: " << inputChar << endl;
+//Variable declaration and initilization for colors
+string cRed = "\033[1;91m";
+string cGreen = "\033[1;92m";
+string cYellow = "\033[1;93m";
+string cBlue = "\033[1;97m\033[44m";
+string cReset = "\033[0m";
 
-	/****************Extend code to Validate string (follow int pattern, if possible)**********/
-	string inputString = (""); 
-	inputString = validateString(inputString); //validate my string by function call, save that value into inputString
-    cout << "You entered string: " << inputString << endl;
+//Input: Prompt the user for number 1 and validate for double
+cout << cGreen << "Please enter Number 1 \n" << cReset << endl;
+number1 = validateDouble(number1);
 
-	/****************Extend code to Validate if truly an int and greater than 77 **********/
-	int input2 = 0;
-	
-	do
-	{
-		cout << "Enter an integer greater than 77." << endl;  
-		input2 = validateInt(input2); //validate my integer by function call, save that value into input
-		
-		if(input2 < 77)
-		{
-			cout << "\nInvalid input: (" << input2 <<") number must be greater than 77." << endl;
-		}
-	}
-	while(input2 < 77); //loop until input2 is greather than 77
+//Input: Prompt the user for number 2 and validate.  Also, ensure it is not 0
+while (number2 == 0.0)
+{
+  cout << cGreen << "\nPlease enter Number 2 (cannot be 0) \n" << cReset << endl;
+  number2 = validateDouble(number2);
 
-    cout << "You entered valid int: " << input2 << endl;
+  if (number2 == 0.0)
+  {
+    cout << cRed << "\nPlease enter a number other than 0 \a" << cReset << endl;
+  }
+}
 
-	/****************Extend code to Validate if truly an double and greater than 0.00 **********/
-	double inputDouble2 = 0.0;
-	
-	do
-	{
-		cout << "Enter an number greater than 0."<< endl; 
-		inputDouble2 = validateDouble(inputDouble2); //validate my integer by function call, save that value into input
-		
-		if(inputDouble2 < 0)
-		{
-			cout << "\nInvalid input: (" << inputDouble2 <<") number must be greater than 0." << endl;
-		}
-	}
-	while(inputDouble2 < 0); //loop until inputDouble2 is greather than 0
+//Input: Prompt the user for numer 3 and validate
+cout << cGreen << "\nPlease enter Number 3 \n" << cReset << endl;
+number3 = validateInt(number3);
 
-    cout << "You entered valid double: " << inputDouble2<< endl;
-    //A program to output grade values
-   //based upon numberical input of the user yourName: Teacher: Dr_T 8-28-2019
-   double grade = 0.0; 
-   do
-   { // begin do 
-      cout << "\nPlease enter a numerical grade (-1 to exit): ";
-      grade = validateDouble(grade); //accept grade and validate the data type
-      //if condition test
-      if(grade >= 90.0)
-      {
-        cout << "\nThat's an A!" << endl; 
-      }
-      else if(grade >= 80.0 && grade <= 89.9)
-      {
-        cout << "\nThat's a B!" << endl; 
-      }
-      else if(grade >= 70.0 && grade <= 79.9)
-      {
-        cout << "\nThat's a C!" << endl; 
-      }
-      else if(grade >= 60.0 && grade <= 69.9)
-      {
-        cout << "\nYikes. That's a D!" << endl; 
-      }
-      else if(grade < 60.0)
-      {
-        cout << "\nSee you next semester: F" << endl; 
-      }
-      else {cout << "\nInvalid input." << endl;}
+//Input: Prompt the user for number 4 and validate.  Also, ensure it is not 0
+while (number4 == 0)
+{
+  cout << cGreen << "\nPlease enter Number 4 (cannot be 0) \n" << cReset << endl;
+  number4 = validateInt(number4);
 
-   }while(grade != -1); //end do 
-   
-    return 0;
+  if (number4 == 0)
+  {
+    cout << cRed << "\nPlease enter a number other than 0 \a" << cReset << endl;
+  }
+}
+
+//Process and Output
+addition = number1 + number2;
+cout << cYellow << "\nAdding Number 1 and Number 2 is: " << cBlue << addition 
+  << cReset << endl;
+
+subtraction = number1 - number2;
+cout << cYellow << "\nSubtracting Number 1 and Number 2 is: " << cBlue << subtraction 
+  << cReset << endl;
+
+multiplication = number1 * number2;
+cout << cYellow << "\nMultiplying Number 1 and Number 2 is: " << cBlue << multiplication
+  << cReset << endl;
+
+division = number1 / number2;
+cout << cYellow << "\nDividing Number 1 and Number 2 is: " << cBlue << division
+  << cReset << endl;
+
+modulus = number3 % number4;
+cout << cYellow << "\nThe remainder of Number 3 into Number 4 is: " << cBlue << modulus 
+  << cReset << endl;
+
+  return 0;
 }
